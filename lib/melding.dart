7aa30d852b3c2c11e4e94b225c958c7a23794f30
Brent van Malsen <i507960@student.fontys.nl>
2024-04-04 10:52:21 +0200

    Sluitknop en titel gemaakt op de melding pagina

--- lib/melding.dart
+++ lib/melding.dart
@@ -3,10 +3,37 @@ import 'package:flutter/material.dart';
 class MeldingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'melding Page',
-        style: TextStyle(fontSize: 30),
+    return Scaffold(
+      appBar: AppBar(
+        title: SizedBox.shrink(), // Titel verwijderd
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: IconButton(
+            icon: Icon(Icons.close, size: 50), // Grootte aangepast
+            onPressed: () {
+              // Voeg hier code toe om acties uit te voeren wanneer de knop wordt ingedrukt
+            },
+          ),
+        ),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.only(
+            top: 20.0,
+            left: 20.0), // Ruimte bovenaan toegevoegd en naar links uitgelijnd
+        child: Column(
+          crossAxisAlignment:
+              CrossAxisAlignment.start, // Uitlijning gewijzigd naar links
+          children: [
+            Text(
+              'Melding maken',
+              style: TextStyle(
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Roboto',
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
