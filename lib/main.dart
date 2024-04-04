56b8e88a4ad5ba9f1e9f841daf11dab8dbc610e7
Brent van Malsen <i507960@student.fontys.nl>
2024-04-04 10:16:24 +0200

    Nieuwe Flutter versie Git test

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Brent en Jorg',
       theme: ThemeData(
         // This is the theme of your application.
         //
