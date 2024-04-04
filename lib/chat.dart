bb5f83be1574079f46d6ba3cc912c5fa00cda3de
JorgRijdt <jorgvanderijdt5@gmail.com>
2024-04-04 12:12:48 +0200

    Opzet van de chat pagina met filter boxes en deel van de chat gemaakt

--- lib/chat.dart
+++ lib/chat.dart
@@ -9,21 +9,20 @@ class ChatPage extends StatelessWidget {
           children: [
             IconButton(
               icon: Icon(Icons.account_circle),
-              onPressed: () {
-                // Handleer het indrukken van het profielicoontje hier
-              },
-              iconSize: 50, // Maak het profielicoontje groter
-              color: Colors.orange, // Geef het profielicoontje een oranje kleur
+              onPressed: () {},
+              iconSize: 45,
+              color: Colors.orange,
             ),
             Expanded(
               child: Container(
                 decoration: BoxDecoration(
+                  color: Color(0xFFEFEFF0),
                   borderRadius: BorderRadius.circular(20),
                   border: Border.all(
                     color: Colors.grey,
                   ),
                 ),
-                padding: EdgeInsets.symmetric(horizontal: 8),
+                padding: EdgeInsets.symmetric(horizontal: 4),
                 child: TextField(
                   textAlign: TextAlign.center,
                   decoration: InputDecoration(
@@ -35,21 +34,110 @@ class ChatPage extends StatelessWidget {
             ),
             IconButton(
               icon: Icon(Icons.add),
-              onPressed: () {
-                // Handleer het indrukken van het icoontje voor een nieuwe chat hier
-              },
-              iconSize: 40, // Maak het icoontje voor een nieuwe chat groter
-              color: Colors
-                  .orange, // Geef het icoontje voor een nieuwe chat een oranje kleur
+              onPressed: () {},
+              iconSize: 32,
+              color: Colors.orange,
             ),
           ],
         ),
       ),
-      body: Center(
-        child: Text(
-          'chat Page',
-          style: TextStyle(fontSize: 30),
-        ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        children: [
+          Container(
+            width: double.infinity,
+            height: 87,
+            decoration: BoxDecoration(
+              color: Color(0xFFF5F5F6),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Row(
+              children: [
+                Container(
+                  width: 106,
+                  height: 87,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      bottomLeft: Radius.circular(15),
+                    ),
+                    image: DecorationImage(
+                      image: NetworkImage("https://via.placeholder.com/106x87"),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Poging tot inbraak',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontFamily: 'Roboto',
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        SizedBox(height: 5),
+                        Text(
+                          'Eva de Boer: Zaterdagnacht om 04:00 waren er 2 mannen...',
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 13,
+                            fontFamily: 'Roboto',
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 25,
+                      height: 25,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF68B1E),
+                        shape: BoxShape.circle,
+                      ),
+                      child: Center(
+                        child: Text(
+                          '1',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 13,
+                            fontFamily: 'Roboto',
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      '21-04-2024',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 10,
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.w200,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
