692bff0450fc62127f130c9e5a1d213758a30614
Brent van Malsen <i507960@student.fontys.nl>
2024-04-09 20:34:36 +0200

    'Beschrijving' kop en subtekst gemaakt

--- lib/beschrijving.dart
+++ lib/beschrijving.dart
@@ -5,18 +5,45 @@ class BeschrijvingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const Center(
-        child: Text('Dit is de Beschrijving pagina'),
+      body: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(left: 32, top: 45, right: 32),
+            child: Text(
+              'Beschrijving',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Roboto',
+              ),
+            ),
+          ),
+          // Toegevoegd: Meer witruimte links en rechts voor de tekst onder 'Beschrijving'
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 32),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 8, 0, 16),
+              child: Text(
+                'Beschrijf hier alle relevante details om de situatie zo duidelijk mogelijk te beschrijven. Hoe meer informatie je kunt geven, zoals tijdstippen en betrokken personen, hoe beter de gemeenschap kan reageren op de melding.',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontFamily: 'Roboto',
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: Container(
-        margin: EdgeInsets.only(right: 12),
+        margin: EdgeInsets.only(right: 16),
         width: 130,
         child: ElevatedButton(
           onPressed: () {},
           style: ElevatedButton.styleFrom(
             backgroundColor: Color(0xFFF68B1E),
             foregroundColor: Colors.white,
-            textStyle: const TextStyle(
+            textStyle: TextStyle(
               color: Colors.white,
               fontWeight: FontWeight.bold,
               fontSize: 15,
