a5a491d6108d262f5b218fe930ab7ded75a4ae05
Brent van Malsen <i507960@student.fontys.nl>
2024-04-09 20:19:31 +0200

    'Plaatsen' knop toegevoegd op de beschrijving pagina

--- lib/beschrijving.dart
+++ lib/beschrijving.dart
@@ -4,12 +4,28 @@ class BeschrijvingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Beschrijving'),
-      ),
-      body: Center(
+      appBar: AppBar(),
+      body: const Center(
         child: Text('Dit is de Beschrijving pagina'),
       ),
+      floatingActionButton: Container(
+        margin: EdgeInsets.only(right: 12),
+        width: 130,
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Color(0xFFF68B1E),
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 15,
+            ),
+          ),
+          child: Text('Plaatsen'),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
     );
   }
 }
