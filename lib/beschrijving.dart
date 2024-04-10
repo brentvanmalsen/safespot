8c549d7ea56e1d0fa5dee94997e0f9d34f04cb22
Brent van Malsen <i507960@student.fontys.nl>
2024-04-10 19:02:31 +0200

    Afbeelding uploaden werkend gemaakt en titel voor Locatie delen toegevoegd

--- lib/beschrijving.dart
+++ lib/beschrijving.dart
@@ -8,7 +8,7 @@ class BeschrijvingPage extends StatefulWidget {
 }
 
 class _BeschrijvingPageState extends State<BeschrijvingPage> {
-  File? _selectedImgae;
+  File? _selectedImage;
 
   Future<void> _pickImageFromGallery() async {
     final returnedImage =
@@ -16,7 +16,7 @@ class _BeschrijvingPageState extends State<BeschrijvingPage> {
 
     if (returnedImage != null) {
       setState(() {
-        _selectedImgae = File(returnedImage.path);
+        _selectedImage = File(returnedImage.path);
       });
     }
   }
@@ -24,7 +24,9 @@ class _BeschrijvingPageState extends State<BeschrijvingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        elevation: 0,
+      ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -53,69 +55,103 @@ class _BeschrijvingPageState extends State<BeschrijvingPage> {
                 ),
               ),
             ),
+            SizedBox(height: 16),
+            const Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Onderwerp',
+                  labelStyle: TextStyle(color: Colors.grey),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFFF68B1E)),
+                  ),
+                  filled: true,
+                  fillColor: Color(0xFFF5F5F6),
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            const Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: TextField(
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                decoration: InputDecoration(
+                  labelText: 'Wat is er gebeurd?',
+                  labelStyle: TextStyle(color: Colors.grey),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFFF68B1E)),
+                  ),
+                  filled: true,
+                  fillColor: Color(0xFFF5F5F6),
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 32),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              child: Row(
                 children: [
-                  const SizedBox(height: 16),
-                  const TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Onderwerp',
-                      labelStyle: TextStyle(color: Colors.grey),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Color(0xFFF68B1E)),
+                  MaterialButton(
+                    color: Color(0xFFF68B1E),
+                    onPressed: _pickImageFromGallery,
+                    child: const Text(
+                      "Bestand uploaden",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
                       ),
-                      filled: true,
-                      fillColor: Color(0xFFF5F5F6),
                     ),
                   ),
-                  const SizedBox(height: 16),
-                  const TextField(
-                    keyboardType: TextInputType.multiline,
-                    maxLines: null,
-                    decoration: InputDecoration(
-                      labelText: 'Wat is er gebeurd?',
-                      labelStyle: TextStyle(color: Colors.grey),
-                      // Aanpassen van de randkleur naar oranje
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Color(0xFFF68B1E)),
-                      ),
-                      filled: true,
-                      fillColor: Color(0xFFF5F5F6),
+                  SizedBox(width: 20),
+                  if (_selectedImage == null)
+                    const Row(
+                      children: [
+                        Icon(Icons.camera_alt),
+                        SizedBox(width: 10),
+                        Icon(Icons.videocam),
+                      ],
                     ),
-                  ),
-                  const SizedBox(height: 16),
-                  Row(
-                    children: [
-                      MaterialButton(
-                        color: Color(0xFFF68B1E),
-                        child: const Text(
-                          "Bestand uploaden",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16),
-                        ),
-                        onPressed: _pickImageFromGallery,
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      if (_selectedImgae != null)
-                        Image.file(_selectedImgae!)
-                      else
-                        const Text("Tekst")
-                    ],
-                  ),
                 ],
               ),
             ),
+            if (_selectedImage != null)
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 32, top: 32, right: 32, bottom: 0),
+                child: Image.file(_selectedImage!),
+              ),
+            const Padding(
+              padding: const EdgeInsets.only(left: 32, top: 45, right: 32),
+              child: Text(
+                'Locatie delen',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Roboto',
+                ),
+              ),
+            ),
+            const Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(0, 8, 0, 16),
+                child: Text(
+                  'Kies ervoor om een locatie toe te voegen die relevant is aan de informatie van je melding. Dit helpt andere buurtbewoners om  een beter beeld te krijgen van mogelijke gevaren of hulpbehoeftes in de omgeving.',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontFamily: 'Roboto',
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
           ],
         ),
       ),
       floatingActionButton: Container(
-        margin: const EdgeInsets.only(right: 16),
+        margin: EdgeInsets.only(right: 16),
         width: 130,
         child: ElevatedButton(
           onPressed: () {},
@@ -128,7 +164,7 @@ class _BeschrijvingPageState extends State<BeschrijvingPage> {
               fontSize: 15,
             ),
           ),
-          child: const Text('Plaatsen'),
+          child: Text('Plaatsen'),
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
