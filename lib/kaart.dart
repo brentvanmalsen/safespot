6249520d3faa5735e57caea5a87bea8d4925f117
JorgRijdt <jorgvanderijdt5@gmail.com>
2024-04-17 09:25:19 +0200

    Google maps geimplemteerd

--- lib/kaart.dart
+++ lib/kaart.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class KaartPage extends StatelessWidget {
   final String location;
@@ -8,15 +9,26 @@ class KaartPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Coördinaten omzetten naar decimale graden
+    double latitude = 51.4506667;
+    double longitude = 5.4559722;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Location'),
       ),
-      body: Center(
-        child: Text(
-          'Location: $location',
-          style: TextStyle(fontSize: 18.0),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(latitude, longitude), // Coördinaten van de locatie
+          zoom: 15.0,
         ),
+        markers: {
+          Marker(
+            markerId: MarkerId('Marker'),
+            position: LatLng(latitude, longitude), // Coördinaten van de locatie
+            infoWindow: InfoWindow(title: 'Location'),
+          ),
+        },
       ),
     );
   }
